cf2dc7b73c3aa471e463b9a1fe6f92f0ac7f8c58
JavaRatXIII <tamannaqureshi9@gmail.com>
2017-07-10 23:56:08 +0100

    Added new data to the database and other minor changes

---
+++ src/mp3player/SongControl.java
@@ -0,0 +1,106 @@
+package mp3player;
+
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+import javazoom.jl.player.*;
+import javazoom.jl.decoder.*;
+/**
+ *
+ * @author Jun
+ */
+public class SongControl implements SongInterface{
+    FileInputStream _fis;
+    BufferedInputStream _bis;
+    String _path;
+    boolean _paused = true;
+    
+    public Player PPlayer;
+    public long PausePosition;
+    public long SongLength;
+    
+    @Override
+    public void New_Song()
+    {
+        Stop();
+        JFileChooser filechooser = new JFileChooser();
+        int result = filechooser.showOpenDialog(null);
+        File file = filechooser.getSelectedFile();
+        _path = file.getAbsolutePath();
+        //_path = "C:\\Users\\Jun\\Music\\Fullmetal_alchemist_ending_1.mp3";
+        
+        _paused = false;
+        Resume();
+    }
+    
+    @Override
+    public void Stop()
+    {
+        if(PPlayer != null)
+        {
+            PPlayer.close();
+            PausePosition = 0;
+            SongLength = 0;
+        }
+    }
+    
+    @Override
+    public void Pause()
+    {
+        try 
+        {
+            _paused = true;
+            PausePosition = _fis.available();
+            PPlayer.close();
+        } 
+        catch (IOException ex) 
+        {
+            System.out.println(ex);
+        }
+    }
+    
+    @Override
+    public void Rewind()
+    {
+        Stop();
+        _paused = false;
+        Resume();
+    }
+    
+    @Override
+    public void Resume()
+    {
+        try 
+        {
+            _fis = new FileInputStream(_path);
+            _bis = new BufferedInputStream(_fis);
+            PPlayer = new Player(_bis);
+            SongLength = _fis.available();
+            System.out.println(SongLength);
+            
+            if(_paused)
+            {
+                _fis.skip(SongLength-PausePosition);
+            }
+        }
+        catch (Exception ex) 
+        {
+            System.out.println(ex);
+        }
+        new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try 
+                {
+                    PPlayer.play();
+                } 
+                catch (JavaLayerException ex) 
+                {
+                    System.out.println(ex);
+                }
+            }
+        }.start();
+    }
+}
---
+++ src/mp3player/SongInterface.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mp3player;
+
+/**
+ *
+ * @author Jun
+ */
+public interface SongInterface {
+    public void New_Song();
+    
+    public void Stop();
+    
+    public void Pause();
+    
+    public void Resume();
+    
+    public void Rewind();
+}
